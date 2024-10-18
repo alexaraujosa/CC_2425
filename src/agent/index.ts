9d563851b118f2f47e36e155bffb04f41bcf5829
alexaraujosa <alexaraujosa@hotmail.com>
2024-10-18 19:04:12 +0100

    feat(server/agent): added TCP sockets
    Implemented initial connection between agent and server via TCP sockets.

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -5,6 +5,10 @@
  */
 
 import { TestType } from "$common/index.js";
+import net from 'net';
+
+const HOST = "127.0.0.1";
+const PORT = "2022";
 
 /**
  * Example agent function with documentaton.
@@ -13,6 +17,24 @@ import { TestType } from "$common/index.js";
  */
 export function agentInit(param1: string, param2: TestType) {
     console.log("Hello world from AGENT.");
+
+    const client = net.connect({port: Number(PORT), host: HOST}, () => {
+        console.log("Connected to " + HOST + ":" + PORT);
+        client.write("Hello server! I'm an agent.");
+
+        client.on("data", data => {
+            console.log("Received data: " + data);
+        })
+
+        client.on("error", error => {
+            console.log(error);
+        })
+
+        client.on("close", () => {
+            console.log("Connection closed.");
+        })
+    });
+
 }
 
 agentInit("def", { prop1: false, prop2: 987, prop3: { prop1: true } });
--- src/server/index.ts
+++ src/server/index.ts
@@ -5,11 +5,15 @@
  */
 
 import path from "path";
+import net  from "net";
 import { TestType } from "$common/index.js";
 import isBinMode from "$common/util/isBinMode.js";
 import { readJsonFile } from "$common/util/paths.js";
 import { Config } from "./config.js";
 import { getOrCreateGlobalLogger } from "$common/util/logger.js";
+
+const HOST = "127.0.0.1";
+const PORT = "2022";
 
 /**
  * Example server function with documentaton.
@@ -17,11 +21,38 @@ import { getOrCreateGlobalLogger } from "$common/util/logger.js";
  * *Also* ~~Supports~~ **Markdown**
  */
 export async function serverInit(param1: string, param2: TestType) {
-    const logger = getOrCreateGlobalLogger({printCallerFile: true, debug: true});
+    const logger = getOrCreateGlobalLogger({printCallerFile: true, debug: false});
     logger.info("Hello world from SERVER.");
 
+    // Config loader
     const json = await readJsonFile<Config>(path.join(process.cwd(), "/tmp/settings.json"));
     logger.info(json);
+
+    // Server setup
+    const server = net.createServer();
+    
+    server.on("connection", socket => {
+        console.log("Agent connected.");
+        socket.write("Hello agent, I'm the server.");
+        socket.write("See you on the other side.");
+        socket.end();
+
+        socket.on("data", data => {
+            console.log("Received data: " + data);
+        });
+
+        socket.on("error", error => {
+            console.log(error);
+            server.close();
+        })
+
+        socket.on("close", () => {
+            console.log("Agent disconnected.")
+        });
+        
+    });
+
+    server.listen({port: PORT, host: HOST}, () => console.log("Server bound to port " + PORT + " with success."));  
 }
 
 if (isBinMode(import.meta.url)) {
