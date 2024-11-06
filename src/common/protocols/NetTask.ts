348e34606190af3657c5909c52148dfc958d6543
alexaraujosa <alexaraujosa@hotmail.com>
2024-11-06 14:47:49 +0000

    feat(protocols): added serializable methods for NetTask Protocol

--- src/common/protocols/NetTask.ts
+++ src/common/protocols/NetTask.ts
@@ -7,7 +7,11 @@
  * @copyright Copyright (c) 2024 DarkenLM https://github.com/DarkenLM
  */
 
+import { BufferReader, BufferWriter } from "$common/util/buffer.js";
+import { getOrCreateGlobalLogger } from "$common/util/logger.js";
+
 const NET_TASK_VERSION = 1;
+const NET_TASK_SIGNATURE = Buffer.from("NTTK", "utf8");
 
 enum NetTaskDatagramType {
     REQUEST_REGISTER,
@@ -56,6 +60,41 @@ class NetTask {
                 "  ACKNOWLEDGEMENT_NUMBER: " + this.acknowledgementNumber + "\n" +
                 "  TYPE: " + this.type + "\n" +
                 "  PAYLOAD_SIZE: " + this.payloadSize + "\n";
+    }
+
+    public static verifySignature(reader: BufferReader): boolean {
+        const sig = reader.read(4);
+
+        return NET_TASK_SIGNATURE.equals(sig);
+    }
+
+    public static readNetTaskDatagram(reader: BufferReader): NetTask {
+        const logger = getOrCreateGlobalLogger();
+        const version = reader.readUInt32();
+        if(version != NET_TASK_VERSION) {
+            logger.pError(`NETTASK Datagram Invalid Version. Excepted: ${NET_TASK_VERSION}. Received: ${version}.`)
+        }
+
+        const agentId = reader.readUInt32();
+        const sequenceNumber = reader.readUInt32();
+        const acknowledgementNumber = reader.readUInt32();
+        const type = reader.readUInt32();
+        const payloadSize = reader.readUInt32();
+
+        return new NetTask(agentId, sequenceNumber, acknowledgementNumber, type, payloadSize);
+    }
+
+    public makeNetTaskDatagram(): Buffer {
+        const writer = new BufferWriter();
+        writer.write(NET_TASK_SIGNATURE);
+        writer.writeUInt32(this.version);
+        writer.writeUInt32(this.agentId);
+        writer.writeUInt32(this.sequenceNumber);
+        writer.writeUInt32(this.acknowledgementNumber);
+        writer.writeUInt32(this.type);
+        writer.writeUInt32(this.payloadSize);
+
+        return writer.finish();
     }
 }
 
