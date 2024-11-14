24bfbb9713b4eb32a2a261c7802e36bed0b50874
DarkenLM <rafaelsantosfernandes660@gmail.com>
2024-11-14 12:52:35 +0000

    fix(server/config): fixed types
    - Made config types actually reflect the config schema.

--- src/server/config.ts
+++ src/server/config.ts
@@ -8,60 +8,60 @@ declare global {
 }
 
 interface DeviceMetrics {
-    cpu_usage: boolean,
-    ram_usage: boolean,
-    interface_stats: boolean,
-    volume: boolean
+    cpu_usage?: boolean,
+    ram_usage?: boolean,
+    interface_stats?: boolean,
+    volume?: boolean
 }
 
 interface GlobalOptions {
-    mode: string,
-    target: string,
-    duration: string,
-    transport: string,
-    interval: string,
-    counter: number
+    mode?: string,
+    target?: string,
+    duration?: string,
+    transport?: string,
+    interval?: string,
+    counter?: number
 }
 
 interface LinkMetrics {
-    bandwith: Bandwith,
-    jitter: Jitter,
-    packet_loss: Packet_loss,
-    latency: Latency
+    bandwith?: Bandwith,
+    jitter?: Jitter,
+    packet_loss?: Packet_loss,
+    latency?: Latency
 }
 
 interface IperfMetrics {
-    mode: string,
-    target: string,
-    duration: string,
-    transport: string,
-    interval: string
+    mode?: string,
+    target?: string,
+    duration?: string,
+    transport?: string,
+    interval?: string
 }
 
 interface Bandwith extends IperfMetrics {}
 interface Jitter extends IperfMetrics {}
 interface Packet_loss extends IperfMetrics {}
 interface Latency {
-    target: string,
-    counter: number,
-    interval: string
+    target?: string,
+    counter?: number,
+    interval?: string
 }
 
 interface AlertConditions {
-    cpu_usage: number,
-    ram_usage: number,
-    interface_stats: number,
-    packet_loss: number,
-    jitter: string
+    cpu_usage?: number,
+    ram_usage?: number,
+    interface_stats?: number,
+    packet_loss?: number,
+    jitter?: string,
+    latency?: string
 }
 
 interface Device {
     ip: string,
-    tasks: String[]
+    tasks: string[]
 }
 
 interface Task {
-    id: string,
     frequency: string,
     device_metrics: DeviceMetrics,
     global_options: GlobalOptions,
@@ -70,8 +70,8 @@ interface Task {
 }
 
 interface Config {
-    tasks: Task[],
-    devices: Device[]
+    tasks: Record<string, Task>,
+    devices: Record<string, Device>
 }
 //#endregion ============== Types ==============
 
