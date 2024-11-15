30e166c6cdd6956c1083f84f7238a11abfe0c63d
Pauloarf <pauloferreira.vnc@gmail.com>
2024-11-15 00:56:41 +0000

    feat(idevice): adds documentation and toString

--- src/db/interfaces/IDevice.ts
+++ src/db/interfaces/IDevice.ts
@@ -1,6 +1,9 @@
 import { Document } from 'mongoose';
 
-// Interface para dispositivos
+/**
+ * Interface representing a Device document in the database.
+ * Extends the Mongoose Document interface to allow direct usage with MongoDB.
+ */
 interface IDevice extends Document {
     id: number,
     ip: string;
@@ -13,6 +16,18 @@ interface IDevice extends Document {
     connectAt: Date;
 }
 
+/**
+ * Creates a new device object with the specified details.
+ *
+ * @param {string} ip - The IP address of the device.
+ * @param {number} port - The port on which the device operates.
+ * @param {Buffer} secret - The secret key for device authentication.
+ * @param {Buffer} salt - The salt value used in cryptographic functions.
+ * @param {Buffer} sessionId - The session ID for the device.
+ * @param {Date} connectAt - The date and time when the device last connected.
+ *
+ * @returns {Partial<IDevice>} A new device object with the specified properties, ready for saving to the database.
+ */
 function createDevice(
     ip: string,
     port: number,
@@ -21,6 +36,7 @@ function createDevice(
     sessionId: Buffer,
     connectAt: Date
 ) { 
+    //TODO: Fazer metodo tostring
     return {
         ip: ip,
         port: port,
@@ -30,6 +46,13 @@ function createDevice(
             sessionId: sessionId,
         },
         connectAt: connectAt,
+
+        toString() {
+            return `Device Information:
+          IP: ${this.ip}
+          Port: ${this.port}
+          Connected At: ${this.connectAt.toISOString()}`;
+        }
     };
 }
 
